b9693a6b9d8ff58c5bed23be641d9b5d6daa84d1
Neil2V <49854178+Neil2V@users.noreply.github.com>
2023-06-13 00:00:14 -0500

    JWT

--- src/main/java/com/example/sintadv1/security/dto/JwtDto.java
+++ src/main/java/com/example/sintadv1/security/dto/JwtDto.java
@@ -2,22 +2,16 @@ package com.example.sintadv1.security.dto;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.security.core.GrantedAuthority;
 
-import java.util.Collection;
 
 @Setter
 @Getter
 public class JwtDto {
 
     private String token;
-    private String bearer = "Bearer";
-    private String username;
-    private Collection<? extends GrantedAuthority> authorities;
 
-    public JwtDto(String token, String username, Collection<? extends GrantedAuthority> authorities) {
+
+    public JwtDto(String token) {
         this.token = token;
-        this.username = username;
-        this.authorities = authorities;
     }
 }
